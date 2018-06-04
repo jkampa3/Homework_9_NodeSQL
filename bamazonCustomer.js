7c2eee074fa93a4ef7ba705c5463fe5addea6a6b
unknown <Joseph.Kampa@gmail.com>
2018-06-04 17:20:41 -0500

    User Prompt Work

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "croEtLu4rO",
     database: "bamazon"
 });
 
@@ -19,7 +19,8 @@ function displayInventory() {
     connection.query(inventoryQuery, function (err, data) {
         if (err) throw err;
 
-        console.log("Initial Inventory At Bamazon:");
+        console.log("---------------------------------------------------------------------------------------------------------------");
+        console.log("                                        -----Current Inventory At Bamazon-----");
         console.log("---------------------------------------------------------------------------------------------------------------\n");
 
         var initialInvenotry = "";
@@ -35,13 +36,60 @@ function displayInventory() {
 
         console.log("---------------------------------------------------------------------------------------------------------------");
 
+        bamazonCustomerPrompt();
+
     });
 
 };
 
+//prompt customer via inquirer for selections
+
+function bamazonCustomerPrompt() {
+
+    console.log("                                          -----BAMAZON PURCHASE CENTER-----");
+    console.log("---------------------------------------------------------------------------------------------------------------\n");
+
+    inquirer.prompt([
+
+        {
+            type: 'input',
+            name: 'item_id',
+            message: 'Please enter the Item ID which you would like to purchase.'
+        },
+        {
+            type: 'input',
+            name: 'quantity',
+            message: 'How many of the above selection do you need?'
+        }
+
+    ]).then(function (userInput) {
+
+        var itemID = input.item_id;
+        var quantityRequested = input.quantity;
+        var inquirerQuery = "SELECT * FROM products where ?";
+
+        connection.query(inquirerQuery,
+            {
+                item_id = item
+            },
+            function (err, data) {
+                if (err) throw err;
+
+                if (data.length === 0) {
+                    console.log('USER ERROR: Selected ITEM ID is invalid. Please select a valid ITEM ID from list below.');
+                    displayInventory();
+                };
+
+
+            });
+
+    });
+
+}
+
 function runBamazonCustomer() {
 
-    displayInventory(); 
+    displayInventory();
 
 };
 
